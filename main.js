62f1d7cf912fe0f8358bfedccad46b82c1b6ae73
RyoSugano <83266893+SuganoRyo@users.noreply.github.com>
2021-05-20 21:17:22 +0900

    Update main.js

--- main.js
+++ main.js
@@ -6,9 +6,10 @@
 
   let startTime;
   let timeoutId;
-
+  let elapsedTime = 0;
+  
   function countUp() {
-    const d = new Date(Date.now() - startTime);
+    const d = new Date(Date.now() - startTime + elapsedTime);
     const m = String(d.getMinutes()).padStart(2, '0');
     const s = String(d.getSeconds()).padStart(2, '0');
     const ms = String(d.getMilliseconds()).padStart(3, '0');
@@ -20,15 +21,41 @@
   }
 
   start.addEventListener('click', () => {
+    setButtonStateRunning();
     startTime = Date.now();
     countUp();
   });
 
   stop.addEventListener('click', () => {
+    setButtonStateStopped();
     clearTimeout(timeoutId);
+    elapsedTime += Date.now() - startTime;
+   console.log(elapsedTime);
   });
 
   reset.addEventListener('click', () => {
+    setButtonStateInitial();
     timer.textContent = '00:00.000';
+    elapsedTime = 0;
   });
 }
+
+function setButtonStateInitial(){
+  start.disabled = false;
+  stop.disabled = true;
+  reset.disabled = true;
+}
+
+function setButtonStateRunning(){
+  start.disabled = true;
+  stop.disabled = false;
+  reset.disabled = true;
+}
+
+function setButtonStateStopped(){
+  start.disabled = false;
+  stop.disabled = true;
+  reset.disabled = false;
+}
+
+setButtonStateInitial();
